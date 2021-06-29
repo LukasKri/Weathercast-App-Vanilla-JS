ea440a0dc5319e4e3ac0ece1b77db91db6a93375
Lukas Krisikaitis <lukaskrisikaitis@gmail.com>
2021-06-29 21:59:23 +0300

    Refactored CSS, minor changes to the JS code + added comments, updated GIF, loading animation may be added latter on.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+// My own OpenWeatherMap API key.
 const API_KEY = "7e2ceebc492d2ed8e785138de97fb8dc";
 
+// Function that handles fetch errors.
 const handleErrors = (response) => {
     if (!response.ok) {
         throw Error(response.statusText);
@@ -7,14 +9,20 @@ const handleErrors = (response) => {
     return response;
 };
 
-const showErrorMessage = (cityName) => {
-    const html = `<h2 class="error-field">Sorry, city "${cityName}" was not found</h2>`;
+// Function that validates user input.
+const handleUserInput = (cityName) => {
+    if (cityName === "") {
+        var html = `<h2 class="error-field">Please enter the name of the city</h2>`;
+    } else {
+        var html = `<h2 class="error-field">Sorry, city "${cityName}" was not found, please try again</h2>`;
+    }
 
-    document.querySelector(".error-field").classList.remove("loading");
-    document.querySelector(".weather").classList.add("loading");
+    document.querySelector(".error-field").classList.remove("do-not-show");
+    document.querySelector(".weather").classList.add("do-not-show");
     document.querySelector(".error-field").innerHTML = html;
 };
 
+// Function that fetches weather from OpenWeatherMap API.
 const fetchWeather = (cityName) => {
     fetch(
         "https://api.openweathermap.org/data/2.5/weather?q=" +
@@ -25,18 +33,23 @@ const fetchWeather = (cityName) => {
         .then(handleErrors)
         .then((response) => response.json())
         .then((data) => displayWeather(data))
-        .catch(() => showErrorMessage(cityName));
+        .catch(() => handleUserInput(cityName));
 };
 
+// Function for weather values rendering in the app.
 const displayWeather = (data) => {
     const { name } = data;
     const { icon, description } = data.weather[0];
     const { temp, humidity } = data.main;
     const { speed } = data.wind;
+    const { country } = data.sys;
 
-    // console.log(name, icon, description, temp, humidity, speed);
+    // console.log(name, icon, description, temp, humidity, speed, country);
 
-    document.querySelector(".city").innerText = "Weather in " + name;
+    const fullCountryName = getCountryName(country);
+
+    document.querySelector(".city").innerText =
+        "Weather in " + name + ", " + fullCountryName;
 
     document.querySelector(".icon").src =
         "http://openweathermap.org/img/wn/" + icon + ".png";
@@ -51,20 +64,281 @@ const displayWeather = (data) => {
     document.querySelector(".wind").innerText =
         "Wind speed: " + speed.toFixed(1) + " m/s";
 
-    document.querySelector(".weather").classList.remove("loading");
+    document.querySelector(".weather").classList.remove("do-not-show");
 
-    document.querySelector(".error-field").classList.add("loading");
+    document.querySelector(".error-field").classList.add("do-not-show");
 };
 
+// Function that takes user input value and passes it to another function.
 const search = () => {
     const searchableCity = document.querySelector(".search-field").value;
     fetchWeather(searchableCity);
 };
 
+// Event listener for search button mouse click.
 document.querySelector(".search button").addEventListener("click", search);
 
+// Event listener for ENTER key press.
 document.querySelector(".search-field").addEventListener("keyup", (event) => {
     if (event.key === "Enter") {
         search();
     }
 });
+
+// Function that transforms country abbreviation to country name.
+const getCountryName = (countryCode) => {
+    if (ISO_COUNTRIES.hasOwnProperty(countryCode)) {
+        return ISO_COUNTRIES[countryCode];
+    } else {
+        return countryCode;
+    }
+};
+
+// List of country abbreviations for country name transformation.
+const ISO_COUNTRIES = {
+    AF: "Afghanistan",
+    AX: "Aland Islands",
+    AL: "Albania",
+    DZ: "Algeria",
+    AS: "American Samoa",
+    AD: "Andorra",
+    AO: "Angola",
+    AI: "Anguilla",
+    AQ: "Antarctica",
+    AG: "Antigua And Barbuda",
+    AR: "Argentina",
+    AM: "Armenia",
+    AW: "Aruba",
+    AU: "Australia",
+    AT: "Austria",
+    AZ: "Azerbaijan",
+    BS: "Bahamas",
+    BH: "Bahrain",
+    BD: "Bangladesh",
+    BB: "Barbados",
+    BY: "Belarus",
+    BE: "Belgium",
+    BZ: "Belize",
+    BJ: "Benin",
+    BM: "Bermuda",
+    BT: "Bhutan",
+    BO: "Bolivia",
+    BA: "Bosnia And Herzegovina",
+    BW: "Botswana",
+    BV: "Bouvet Island",
+    BR: "Brazil",
+    IO: "British Indian Ocean Territory",
+    BN: "Brunei Darussalam",
+    BG: "Bulgaria",
+    BF: "Burkina Faso",
+    BI: "Burundi",
+    KH: "Cambodia",
+    CM: "Cameroon",
+    CA: "Canada",
+    CV: "Cape Verde",
+    KY: "Cayman Islands",
+    CF: "Central African Republic",
+    TD: "Chad",
+    CL: "Chile",
+    CN: "China",
+    CX: "Christmas Island",
+    CC: "Cocos (Keeling) Islands",
+    CO: "Colombia",
+    KM: "Comoros",
+    CG: "Congo",
+    CD: "Congo, Democratic Republic",
+    CK: "Cook Islands",
+    CR: "Costa Rica",
+    CI: "Cote D'Ivoire",
+    HR: "Croatia",
+    CU: "Cuba",
+    CY: "Cyprus",
+    CZ: "Czech Republic",
+    DK: "Denmark",
+    DJ: "Djibouti",
+    DM: "Dominica",
+    DO: "Dominican Republic",
+    EC: "Ecuador",
+    EG: "Egypt",
+    SV: "El Salvador",
+    GQ: "Equatorial Guinea",
+    ER: "Eritrea",
+    EE: "Estonia",
+    ET: "Ethiopia",
+    FK: "Falkland Islands (Malvinas)",
+    FO: "Faroe Islands",
+    FJ: "Fiji",
+    FI: "Finland",
+    FR: "France",
+    GF: "French Guiana",
+    PF: "French Polynesia",
+    TF: "French Southern Territories",
+    GA: "Gabon",
+    GM: "Gambia",
+    GE: "Georgia",
+    DE: "Germany",
+    GH: "Ghana",
+    GI: "Gibraltar",
+    GR: "Greece",
+    GL: "Greenland",
+    GD: "Grenada",
+    GP: "Guadeloupe",
+    GU: "Guam",
+    GT: "Guatemala",
+    GG: "Guernsey",
+    GN: "Guinea",
+    GW: "Guinea-Bissau",
+    GY: "Guyana",
+    HT: "Haiti",
+    HM: "Heard Island & Mcdonald Islands",
+    VA: "Holy See (Vatican City State)",
+    HN: "Honduras",
+    HK: "Hong Kong",
+    HU: "Hungary",
+    IS: "Iceland",
+    IN: "India",
+    ID: "Indonesia",
+    IR: "Iran, Islamic Republic Of",
+    IQ: "Iraq",
+    IE: "Ireland",
+    IM: "Isle Of Man",
+    IL: "Israel",
+    IT: "Italy",
+    JM: "Jamaica",
+    JP: "Japan",
+    JE: "Jersey",
+    JO: "Jordan",
+    KZ: "Kazakhstan",
+    KE: "Kenya",
+    KI: "Kiribati",
+    KR: "Korea",
+    KW: "Kuwait",
+    KG: "Kyrgyzstan",
+    LA: "Lao People's Democratic Republic",
+    LV: "Latvia",
+    LB: "Lebanon",
+    LS: "Lesotho",
+    LR: "Liberia",
+    LY: "Libyan Arab Jamahiriya",
+    LI: "Liechtenstein",
+    LT: "Lithuania",
+    LU: "Luxembourg",
+    MO: "Macao",
+    MK: "Macedonia",
+    MG: "Madagascar",
+    MW: "Malawi",
+    MY: "Malaysia",
+    MV: "Maldives",
+    ML: "Mali",
+    MT: "Malta",
+    MH: "Marshall Islands",
+    MQ: "Martinique",
+    MR: "Mauritania",
+    MU: "Mauritius",
+    YT: "Mayotte",
+    MX: "Mexico",
+    FM: "Micronesia, Federated States Of",
+    MD: "Moldova",
+    MC: "Monaco",
+    MN: "Mongolia",
+    ME: "Montenegro",
+    MS: "Montserrat",
+    MA: "Morocco",
+    MZ: "Mozambique",
+    MM: "Myanmar",
+    NA: "Namibia",
+    NR: "Nauru",
+    NP: "Nepal",
+    NL: "Netherlands",
+    AN: "Netherlands Antilles",
+    NC: "New Caledonia",
+    NZ: "New Zealand",
+    NI: "Nicaragua",
+    NE: "Niger",
+    NG: "Nigeria",
+    NU: "Niue",
+    NF: "Norfolk Island",
+    MP: "Northern Mariana Islands",
+    NO: "Norway",
+    OM: "Oman",
+    PK: "Pakistan",
+    PW: "Palau",
+    PS: "Palestinian Territory, Occupied",
+    PA: "Panama",
+    PG: "Papua New Guinea",
+    PY: "Paraguay",
+    PE: "Peru",
+    PH: "Philippines",
+    PN: "Pitcairn",
+    PL: "Poland",
+    PT: "Portugal",
+    PR: "Puerto Rico",
+    QA: "Qatar",
+    RE: "Reunion",
+    RO: "Romania",
+    RU: "Russian Federation",
+    RW: "Rwanda",
+    BL: "Saint Barthelemy",
+    SH: "Saint Helena",
+    KN: "Saint Kitts And Nevis",
+    LC: "Saint Lucia",
+    MF: "Saint Martin",
+    PM: "Saint Pierre And Miquelon",
+    VC: "Saint Vincent And Grenadines",
+    WS: "Samoa",
+    SM: "San Marino",
+    ST: "Sao Tome And Principe",
+    SA: "Saudi Arabia",
+    SN: "Senegal",
+    RS: "Serbia",
+    SC: "Seychelles",
+    SL: "Sierra Leone",
+    SG: "Singapore",
+    SK: "Slovakia",
+    SI: "Slovenia",
+    SB: "Solomon Islands",
+    SO: "Somalia",
+    ZA: "South Africa",
+    GS: "South Georgia And Sandwich Isl.",
+    ES: "Spain",
+    LK: "Sri Lanka",
+    SD: "Sudan",
+    SR: "Suriname",
+    SJ: "Svalbard And Jan Mayen",
+    SZ: "Swaziland",
+    SE: "Sweden",
+    CH: "Switzerland",
+    SY: "Syrian Arab Republic",
+    TW: "Taiwan",
+    TJ: "Tajikistan",
+    TZ: "Tanzania",
+    TH: "Thailand",
+    TL: "Timor-Leste",
+    TG: "Togo",
+    TK: "Tokelau",
+    TO: "Tonga",
+    TT: "Trinidad And Tobago",
+    TN: "Tunisia",
+    TR: "Turkey",
+    TM: "Turkmenistan",
+    TC: "Turks And Caicos Islands",
+    TV: "Tuvalu",
+    UG: "Uganda",
+    UA: "Ukraine",
+    AE: "United Arab Emirates",
+    GB: "United Kingdom",
+    US: "United States",
+    UM: "United States Outlying Islands",
+    UY: "Uruguay",
+    UZ: "Uzbekistan",
+    VU: "Vanuatu",
+    VE: "Venezuela",
+    VN: "Viet Nam",
+    VG: "Virgin Islands, British",
+    VI: "Virgin Islands, U.S.",
+    WF: "Wallis And Futuna",
+    EH: "Western Sahara",
+    YE: "Yemen",
+    ZM: "Zambia",
+    ZW: "Zimbabwe",
+};
